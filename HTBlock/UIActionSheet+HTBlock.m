0f00be1b759165a45533967bddc9ac490635f446
hardtack <6566gun@gmail.com>
2013-02-11 23:00:32 +0900

    License

--- HTBlock/UIActionSheet+HTBlock.m
+++ HTBlock/UIActionSheet+HTBlock.m
@@ -1,10 +1,21 @@
-//
-//  UIActionSheet+HTBlock.m
-//  meety_ios
-//
-//  Created by 최건우 on 13. 2. 11..
-//  Copyright (c) 2013년 Meety. All rights reserved.
-//
+/*
+ * UIActionSheet+HTBlock.m
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                    Version 2, February 2013
+ *
+ * Copyright (C) 2013 GunWoo Choi
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ *
+ */
 
 #import "UIActionSheet+HTBlock.h"
 #import <objc/runtime.h>
--- HTBlock/UIImagePickerController+HTBlock.h
+++ HTBlock/UIImagePickerController+HTBlock.h
@@ -1,10 +1,21 @@
-//
-//  UIImagePickerController+HTBlock.h
-//  meety_ios
-//
-//  Created by 최건우 on 13. 2. 11..
-//  Copyright (c) 2013년 Meety. All rights reserved.
-//
+/*
+ * UIImagePickerController+HTBlock.h
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                    Version 2, February 2013
+ *
+ * Copyright (C) 2013 GunWoo Choi
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified
+ * copies of this license document, and changing it is allowed as long
+ * as the name is changed.
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+ *
+ *  0. You just DO WHAT THE FUCK YOU WANT TO.
+ *
+ */
 
 #import <UIKit/UIKit.h>
 
